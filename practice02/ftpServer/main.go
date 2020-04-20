1f2d042db823885b0449811cab75dd394c9bcc9d
z15085 <zhang.binbin@h3c.com>
2020-04-20 20:10:38 +0800

    add new ftpServer

--- practice02/ftpServer/main.go
+++ practice02/ftpServer/main.go
@@ -1,1 +1,16 @@
 package main
+
+import (
+	"log"
+	"net/http"
+	"os"
+)
+
+func main() {
+	os.Mkdir("file", 0777)
+
+	http.Handle("/bevisy/", http.StripPrefix("/bevisy/", http.FileServer(http.Dir("file"))))
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal("Server: ", err)
+	}
+}
