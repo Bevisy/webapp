0fd2f799a369ef14a8f4de06bc598a3dca642781
bevisy <binbin36520@gmail.com>
2020-05-10 23:22:30 +0800

    更新获取镜像注释
    Signed-off-by: bevisy <binbin36520@gmail.com>

--- containerdSample/createContainer/createContainer.go
+++ containerdSample/createContainer/createContainer.go
@@ -13,16 +13,21 @@ import (
 
 func main() {
 	path := "/run/containerd/containerd.sock"
+	//新建containerd client
 	client, err := containerd.New(path)
 	defer client.Close()
 	if err != nil {
 		log.Fatal(err)
 	}
+	//指定 containerd 将要使用的命名空间
 	ctx := namespaces.WithNamespace(context.Background(), "example")
-	redisImage := "docker.io/library/redis:alpine"
-	image, err := client.Pull(ctx, redisImage, containerd.WithPullUnpack)
+	cImage := "docker.io/library/busybox:latest"
+	//拉取指定容器镜像，将其解压缩到快照程序中，以用作根文件系统。
+	//（unpack it into a snapshotter for use as a root filesystem.）
+	image, err := client.Pull(ctx, cImage, containerd.WithPullUnpack)
 	if err != nil {
 		panic(err)
 	}
+	//image.Name() 示例：docker.io/library/busybox:latest
 	log.Printf("Successfully pulled %s image\n", image.Name())
 }
