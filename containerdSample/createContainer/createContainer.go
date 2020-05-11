9692dbff956911b1f4c06d9bf896828dcbf6fae6
bevisy <binbin36520@gmail.com>
2020-05-11 21:52:03 +0800

    containerd 创建容器例程
    Signed-off-by: bevisy <binbin36520@gmail.com>

--- containerdSample/createContainer/createContainer.go
+++ containerdSample/createContainer/createContainer.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"github.com/containerd/containerd"
 	"github.com/containerd/containerd/namespaces"
+	"github.com/containerd/containerd/oci"
 	"log"
 )
 
@@ -21,7 +22,7 @@ func main() {
 	}
 	//指定 containerd 将要使用的命名空间
 	ctx := namespaces.WithNamespace(context.Background(), "example")
-	cImage := "docker.io/library/busybox:latest"
+	cImage := "docker.io/library/redis:alpine"
 	//拉取指定容器镜像，将其解压缩到快照程序中，以用作根文件系统。
 	//（unpack it into a snapshotter for use as a root filesystem.）
 	image, err := client.Pull(ctx, cImage, containerd.WithPullUnpack)
@@ -30,4 +31,14 @@ func main() {
 	}
 	//image.Name() 示例：docker.io/library/busybox:latest
 	log.Printf("Successfully pulled %s image\n", image.Name())
+
+	container, err := client.NewContainer(ctx, "redis-server",
+		containerd.WithNewSnapshot("redis-server-snapshot", image),
+		containerd.WithNewSpec(oci.WithImageConfig(image)),
+	)
+	if err != nil {
+		panic(err)
+	}
+	log.Printf("container %s create succeess.", container.ID())
+	defer container.Delete(ctx, containerd.WithSnapshotCleanup)
 }
