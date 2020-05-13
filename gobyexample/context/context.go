765ee6125dd67f87204d42acd71899b99260a72e
bevisy <binbin36520@gmail.com>
2020-05-13 15:24:17 +0800

    add go context example
    Signed-off-by: bevisy <binbin36520@gmail.com>

---
+++ gobyexample/context/context.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+)
+
+func main() {
+	http.HandleFunc("/hello", hello)
+	http.ListenAndServe(":8080", nil)
+}
+
+func hello(w http.ResponseWriter, req *http.Request) {
+	ctx := req.Context()
+	log.Println("server: hello handler started.")
+	defer log.Println("server: hello handler ended.")
+
+	select {
+	case <-time.After(10 * time.Second):
+		fmt.Fprintf(w, "hello\n")
+	case <-ctx.Done():
+
+		err := ctx.Err()
+		log.Println("server: ", err)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}
